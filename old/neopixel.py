3136c5af7bc9af3d9a8a384f5da5c35592965262
BrorH <bahjemga@uio.no>
2022-01-12 15:06:22 +0100

    moved old file

---
+++ old/neopixel.py
@@ -0,0 +1,14 @@
+
+class pseudoList(list):
+    
+    def fill(*args, **kwargs):
+        return
+    
+    def show(*args, **kwargs):
+        return
+
+def GRB():
+    pass
+
+def NeoPixel(*args, **kwargs):
+    return pseudoList(range(240))
