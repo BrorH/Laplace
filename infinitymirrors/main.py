927e7460dbf5fcd3be1bd5791d85ea24aa589085
brorH <bror.hjemgaard@gmail.com>
2022-01-13 18:29:45 +0100

    updated main structure

--- infinitymirrors/main.py
+++ infinitymirrors/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 
-
 """
 Made by Bror Hjemgaard 2021 for Laplace, UiO
 Bror.hjemgaard@gmail.com
@@ -163,115 +162,11 @@ class Lightshow:
             self.pixels.brightness = analog/1023 if sw else 0
         
 
-    # def log_error(self, err):
-    #     print("got error:")
-    #     print(err)
-    #     print("===================")
-    #     # with open("home/pi/Laplace/err.txt", "w+") as file:
-    #     with open("./err.txt", "w+") as file:
-    #         file.writelines(err)
-
-# def wheel(pos):
-#         # Input a value 0 to 255 to get a color value.
-#         # The colours are a transition r - g - b - back to r.
-#         if pos < 0 or pos > 255:
-#             r = g = b = 0
-#         elif pos < 85:
-#             r = int(pos * 3)
-#             g = int(255 - pos * 3)
-#             b = 0
-#         elif pos < 170:
-#             pos -= 85
-#             r = int(255 - pos * 3)
-#             g = 0
-#             b = int(pos * 3)
-#         else:
-#             pos -= 170
-#             r = 0
-#             g = int(pos * 3)
-#             b = int(255 - pos * 3)
-#         return (r, g, b)
-
-
-# def rainbow_cycle(id = 0):
-
-    
-#     while (not kill_all_threads):# and (not sw):
-#         for j in range(255):
-#             if (kill_all_threads): break
-#             for i in range(len(pixels)):
-#                 pixel_index = (i * 256 // len(pixels)) + j
-#                 pixels[i] = wheel(pixel_index & 255)
-#             pixels.show()
-            
-#             if lightshow_id == id: # check if button has been pressed
-#                 time.sleep(0.01)
-#             else:
-#                 return
-
-
-# def fade(id = 1):
-#     while (not kill_all_threads):# and (not sw):
-#         color = [random.randint(0,255) for i in range(3)]
-#         color[random.randint(0,2)] = 0
-#         for i in range(256):
-#             pixels.fill(tuple([int(i/256 * a) for a in color]))
-#             pixels.show()
-#             if lightshow_id == id: # check if button has been pressed
-#                 time.sleep(0.005)
-#             else:
-#                 return
-#         for i in range(int(256/2)):
-#             i = 255 - i*2
-#             pixels.fill(tuple([int(i/256 * a) for a in color]))
-#             pixels.show()
-#             if lightshow_id == id: # check if button has been pressed
-#                 time.sleep(0.005)
-#             else:
-#                 return
-#         pixels.fill((0,0,0))
-#         pixels.show()
-#         for i in range(10):
-#             if lightshow_id == id: # check if button has been pressed
-#                 time.sleep(0.01)
-#             else:
-#                 return
-
-
-# def circus(id = 2):
-#     while (not kill_all_threads):# and (not sw):
-#         j = 0
-#         L = 30
-#         for i in range(120):
-#             newcolor = wheel(int((4*i+0*j*120)/240/2*255))
-#             for k in range(L):
-#                 newERcolor = tuple([ int(a*(1-math.sqrt((k-L/2)**2)/(L/2))) for a in newcolor])
-#                 pixels[(i+k)%120] = newERcolor
-#                 # pixels[(i+k)%120].brightness = (k-9)**2/81
-#                 pixels[239-(i+6+k)%120] = newERcolor
-#             pixels[(i-1)%120] = (0,0,0)
-
-#             # pixels[239-(i+6+1)%120] = newcolor
-#             # pixels[239-(i+6+2)%120] = newcolor
-#             pixels[239-((i-1)+6)%120] = (0,0,0)
-
-#             pixels.show()
-#             if lightshow_id == id: # check if button has been pressed
-#                 time.sleep(0.01)
-#             else:
-#                 return
-#         j = (j+1)%2
-
 
 if __name__ == "__main__":
-    # wait for button press to change lightshow_id
-    #lightshows = [rainbow_cycle,fade, circus]
-    
     ligthshow = Lightshow()
     ligthshow.start()
 
-
-    # num_lightshows = len(lightshows) # the number of different lightshows
 
 
     
